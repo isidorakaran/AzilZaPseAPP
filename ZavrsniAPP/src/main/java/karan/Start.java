ab0ffc4e9bbf965efc40e752339348af18d07cd9
isidorakaran <karanisidora@hotmail.com>
2023-03-11 18:33:35 +0100

    Uspješna provjera

---
+++ ZavrsniAPP/src/main/java/karan/Start.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package karan;
+
+import karan.util.HibernateUtil;
+
+/**
+ *
+ * @author WinUSER
+ */
+public class Start {
+
+    public static void main(String[] args) {
+        HibernateUtil.getsesSession();
+    }
+}
