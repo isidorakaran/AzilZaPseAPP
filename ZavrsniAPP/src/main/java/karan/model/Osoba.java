f22a85c8aab094f8254180b3a759038f91fa666f
isidorakaran <karanisidora@hotmail.com>
2023-03-11 18:32:29 +0100

    Dovršene sve klase u modelu

---
+++ ZavrsniAPP/src/main/java/karan/model/Osoba.java
@@ -0,0 +1,61 @@
+package karan.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Osoba extends Entitet {
+
+    private String ime;
+    private String prezime;
+    private String email;
+    private String brojTelefona;
+
+    public Osoba() {
+        super();
+    }
+
+    public Osoba(int sifra, String ime, String prezime, String email, String brojTelefona) {
+        super(sifra);
+        this.ime = ime;
+        this.prezime = prezime;
+        this.email = email;
+        this.brojTelefona = brojTelefona;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBrojTelefona() {
+        return brojTelefona;
+    }
+
+    public void setBrojTelefona(String brojTelefona) {
+        this.brojTelefona = brojTelefona;
+    }
+
+    @Override
+    public String toString() {
+        return ime + " " + prezime;
+    }
+}
---
+++ ZavrsniAPP/src/main/java/karan/model/Transakcija.java
@@ -0,0 +1,77 @@
+package karan.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import java.util.Date;
+@Entity
+public class Transakcija extends Entitet {
+
+    private Date datum;
+    private String opis;
+    private String napomena;
+    @ManyToOne
+    private Osoba osoba;
+    @ManyToOne
+    private VrstaTransakcije vrstaTransakcije;
+
+    public Transakcija() {
+        super();
+    }
+
+    public Transakcija(int sifra, Date datum, String opis, String napomena, Osoba osoba,
+            VrstaTransakcije vrstaTransakcije) {
+        super(sifra);
+        this.datum = datum;
+        this.opis = opis;
+        this.napomena = napomena;
+        this.osoba = osoba;
+        this.vrstaTransakcije = vrstaTransakcije;
+    }
+
+    public Date getDatum() {
+        return datum;
+    }
+
+    public void setDatum(Date datum) {
+        this.datum = datum;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+    public String getNapomena() {
+        return napomena;
+    }
+
+    public void setNapomena(String napomena) {
+        this.napomena = napomena;
+    }
+
+    public Osoba getOsoba() {
+        return osoba;
+    }
+
+    public void setOsoba(Osoba osoba) {
+        this.osoba = osoba;
+    }
+
+    public VrstaTransakcije getVrstaTransakcije() {
+        return vrstaTransakcije;
+    }
+
+    public void setVrstaTransakcije(VrstaTransakcije vrstaTransakcije) {
+        this.vrstaTransakcije = vrstaTransakcije;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return datum + opis + napomena;
+    }
+
+}
