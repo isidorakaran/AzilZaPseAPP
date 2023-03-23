e69b15a5304ecc26397558c0d4d302220bed8372
isidorakaran <karanisidora@hotmail.com>
2023-03-23 19:26:59 +0100

    Menjani podaci sa fakera

--- ZavrsniAPP/src/main/java/karan/util/PocetniInsert.java
+++ ZavrsniAPP/src/main/java/karan/util/PocetniInsert.java
@@ -19,11 +19,11 @@ import org.hibernate.Session;
  * @author WinUSER
  */
 public class PocetniInsert {
-    
-    private static int BROJ_OSOBA=20;
-    private static int BROJ_PASA=50;
-    private static int BROJ_TRANSAKCIJA=50;
-    private static int BROJ_VRSTATRANSAKCIJE=2;
+
+    private static int BROJ_OSOBA = 20;
+    private static int BROJ_PASA = 50;
+    private static int BROJ_TRANSAKCIJA = 50;
+    private static int BROJ_VRSTATRANSAKCIJE = 2;
 
     private Faker faker;
     private List<Osoba> osobe;
@@ -38,82 +38,78 @@ public class PocetniInsert {
         psi = new ArrayList<>();
         transakcije = new ArrayList<>();
         vrsteTransakcije = new ArrayList<>();
-        session=HibernateUtil.getsesSession();
+        session = HibernateUtil.getsesSession();
         session.beginTransaction();
         kreirajOsobe();
         kreirajVrsteTransakcije();
         kreirajTransakcije();
         kreirajPse();
-        
+
         session.getTransaction().commit();
     }
 
     private void kreirajOsobe() {
         Osoba o;
-        for(int i=0;i<BROJ_OSOBA;i++){
-            o=new Osoba();
+        for (int i = 0; i < BROJ_OSOBA; i++) {
+            o = new Osoba();
             o.setIme(faker.name().firstName());
             o.setPrezime(faker.name().lastName());
             o.setEmail(faker.internet().emailAddress());
-            o.setBrojTelefona(faker.internet().ipV6Address());
+            o.setBrojTelefona(faker.phoneNumber().cellPhone());
             session.persist(o);
             osobe.add(o);
         }
-        
+
     }
 
     private void kreirajVrsteTransakcije() {
-          VrstaTransakcije vt;
-        for(int i=0;i<BROJ_VRSTATRANSAKCIJE;i++){
-            vt=new VrstaTransakcije();
-            vt.setNaziv(faker.name().prefix());
+        VrstaTransakcije vt;
+        for (int i = 0; i < BROJ_VRSTATRANSAKCIJE; i++) {
+            vt = new VrstaTransakcije();
+            vt.setNaziv("in");
+            vt.setNaziv("out");
             session.persist(vt);
             vrsteTransakcije.add(vt);
         }
     }
 
     private void kreirajTransakcije() {
-         Transakcija t;
-        for(int i=0;i<BROJ_TRANSAKCIJA;i++){
-            t=new Transakcija();
+        Transakcija t;
+        for (int i = 0; i < BROJ_TRANSAKCIJA; i++) {
+            t = new Transakcija();
             t.setDatum(faker.date().birthday());
             t.setOpis(faker.harryPotter().quote());
             t.setNapomena(faker.lordOfTheRings().character());
-            t.setOsoba(osobe.get(sb(0,BROJ_OSOBA-1)));
+            t.setOsoba(osobe.get(sb(0, BROJ_OSOBA - 1)));
             t.setVrstaTransakcije(vrsteTransakcije.get(sb(0, BROJ_VRSTATRANSAKCIJE - 1)));
             session.persist(t);
             transakcije.add(t);
+        }
+
     }
-    
-   
-    
-}
+
     private void kreirajPse() {
         Pas p;
         List<Transakcija> t;
-        for(int i=0;i<BROJ_PASA;i++){
-            p=new Pas();
+        for (int i = 0; i < BROJ_PASA; i++) {
+            p = new Pas();
             p.setIme(faker.dog().name());
             p.setDob(faker.number().randomDigitNotZero());
             //p.setKilaza(faker.number().);
             p.setMjesavina(faker.bool().bool());
             p.setPol(faker.dog().gender());
-            t=new ArrayList<>();
-            for(int j=0;j<sb(1, 2);j++){
-                t.add(transakcije.get(sb(0, BROJ_TRANSAKCIJA-1)));
+            t = new ArrayList<>();
+            for (int j = 0; j < sb(1, 2); j++) {
+                t.add(transakcije.get(sb(0, BROJ_TRANSAKCIJA - 1)));
             }
             p.setTransakcije(t);
             session.persist(p);
-            
+
         }
     }
-    
-    
-    
-    
-    private int sb(int min,int max){
+
+    private int sb(int min, int max) {
         return ThreadLocalRandom.current().nextInt(min, max + 1);
     }
 
-    
 }
