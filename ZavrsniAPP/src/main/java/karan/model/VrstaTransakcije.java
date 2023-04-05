f46274bdc856348c27800881df6b3c00cc017c3f
isidorakaran <karanisidora@hotmail.com>
2023-04-05 14:22:27 +0200

    Dodan atribut predznak

--- ZavrsniAPP/src/main/java/karan/model/VrstaTransakcije.java
+++ ZavrsniAPP/src/main/java/karan/model/VrstaTransakcije.java
@@ -6,15 +6,26 @@ import jakarta.persistence.Entity;
 public class VrstaTransakcije extends Entitet {
 
     private String naziv;
+    private int predznak;
 
     public VrstaTransakcije() {
         super();
     }
 
-    public VrstaTransakcije(int sifra, String naziv) {
+    public VrstaTransakcije(int sifra, String naziv,int predznak) {
         super(sifra);
         this.naziv = naziv;
+        this.predznak=predznak;
     }
+
+    public int getPredznak() {
+        return predznak;
+    }
+
+    public void setPredznak(int predznak) {
+        this.predznak = predznak;
+    }
+    
 
     public String getNaziv() {
         return naziv;
